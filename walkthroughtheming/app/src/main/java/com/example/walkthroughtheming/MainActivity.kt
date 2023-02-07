4d091ca70ae1ed83d76e4ce1a3cefc58b2d19024
Nisha Kumari <89996949+t1kuni01@users.noreply.github.com>
2023-02-07 14:30:43 +0200

    first commit

---
+++ walkthroughtheming/app/src/main/java/com/example/walkthroughtheming/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.walkthroughtheming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.walkthroughtheming.ui.theme.WalkthroughThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WalkthroughThemingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val appModifier = Modifier.fillMaxWidth().padding(8.dp)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        Text(
+            text = "My tittle",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(
+            value ="",
+            onValueChange = { /*TODO*/},
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/},
+            modifier = appModifier
+                ){
+            Text(text="Submit")
+
+
+        }
+
+    }
+}
